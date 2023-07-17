ab5be7d4a1dc007a38b5e85a69eb48161fd983fa
mmm999xp <mmm999xp@gmail.com>
2023-07-18 01:25:11 +0800

    新增handlebars，模板網站完成

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const express = require('express')
+const exphbs = require('express-handlebars')
 const app = express()
 
+//設定渲染引擎
+app.engine('hbs',exphbs({ defaultLayout:'main',extname:'.hbs'}))
+app.set('view engine' , 'hbs')
+
+
 app.get('/', (req,res)=>{
-  res.send('hello world')
+  res.render('index')
 })
 
 app.listen(3000,()=>{
