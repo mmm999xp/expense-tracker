b77458de91339e53012242d45415671bc80a3cda
mmm999xp <mmm999xp@gmail.com>
2023-07-23 00:36:05 +0800

    運用bcrypt 配對密碼登入完成

--- config/passport.js
+++ config/passport.js
@@ -1,6 +1,7 @@
 const passport = require('passport')
 const LocalStrategy = require('passport-local').Strategy
 const User = require('../models/user')
+const bcrypt = require('bcryptjs')
 module.exports = app => {
   // 初始化 Passport 模組
   app.use(passport.initialize())
@@ -9,13 +10,17 @@ module.exports = app => {
   passport.use(new LocalStrategy({ usernameField: 'email' }, (email, password, done) => {
     User.findOne({ email })
       .then(user => {
-        if (!user) {
-          return done(null, false, { message: 'That email is not registered!' })
-        }
-        if (user.password !== password) {
-          return done(null, false, { message: 'Email or Password incorrect.' })
-        }
-        return done(null, user)
+        //檢測email是否被註冊過已經在routes/modules/users檢測過了
+
+
+        //return done(null, user)
+        return bcrypt.compare(password, user.password).then(isMatch => {
+          //檢測帳號與密碼是否正確已經在routes/modules/users檢測過了
+           if (!isMatch) {
+             return done(null, false)
+           }
+          return done(null, user)
+        })
       })
       .catch(err => done(err, false))
   }))
